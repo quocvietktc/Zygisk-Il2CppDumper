07f8b5a6d38522e8562c19fdad7e1ba129d7ae59
quocvietktc <qvietktc@gmail.com>
2025-11-24 11:15:55 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -1,7 +1,10 @@
 //
 // Created by Perfare on 2020/7/4.
 //
+#pragma once
 
+// Đây là chuỗi "mồi". GitHub Actions sẽ tìm chữ này để thay thế.
+static const char *GamePackageName = "com.game.packagename";
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
